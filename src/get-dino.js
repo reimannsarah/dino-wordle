1b62900f1b87a202a62753f189991384a32cf641
Sarah Reimann <reimannsarah45@gmail.com>
2023-04-18 12:07:20 -0700

    made it
    co-authored-by: Kyle Crawford <kcraw217@gmail.com>

---
+++ src/get-dino.js
@@ -0,0 +1,18 @@
+export default class Dino {
+  static getDino() {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `https://dinoipsum.com/api/?format=json&paragraphs=1&words=1`;
+      request.addEventListener("loadend", function(){
+        const response = JSON.parse(this.responseText);
+        if (this.status === 200) {
+          resolve(response);
+        } else {
+          reject ([this,response]);
+        }
+      });
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,63 @@
-import './css/styles.css';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import Dino from './get-dino.js';
+
+//Business Logic
+
+function getDino() {
+  let promise = Dino.getDino();
+  promise.then(function (dino) {
+    printDino(dino);
+  }, function (errorDino) {
+    printError(errorDino);
+  });
+}
+
+//UI Logic
+
+function printDino(dino) {
+  document.querySelector('#show-dino').innerText = dino;
+  let dinoArray = dino[0][0].toUpperCase().split("");
+  console.log(dinoArray);
+  const regex = /[AEIUO]/;
+  for (let i = 0; i < dinoArray.length; i++) {
+    if (regex.test(dinoArray[i])) {
+      let div = document.getElementById("letters");
+      let span = document.createElement("p");
+      span.setAttribute("class", "letters");
+      span.innerText = dinoArray[i];
+      div.append(span);
+    } else {
+      let form = document.getElementById("form");
+      let input = document.createElement("input");
+      input.setAttribute("class", "letters");
+      input.setAttribute("placeholder", dinoArray[i]);
+      form.append(input);
+    }
+  }
+
+  //   dinoArray.forEach((element) => {
+  //     if() {
+  //       console.log(typeof element);
+  //       let form = document.getElementById("form");
+  //       let input = document.createElement("input");
+  //       input.setAttribute("class", "letters");
+  //       input.setAttribute("placeholder", element)
+  //       form.append(input);
+  //     }else {
+  // let div = document.getElementById("letters");
+  // let span = document.createElement("span");
+  // span.setAttribute("class", "letters");
+  // div.append(span);
+  //     }
+  //   });
+  //   console.log(dinoArray);
+}
+
+function printError(errorDino) {
+  document.querySelector('#showDino').innerText = `There was a jurassic error accessing the dino data: ${errorDino[0].status} ${errorDino[0].statusText}: ${errorDino[1].message}`;
+}
+
+
+getDino();
